6f2de9dad4459a3169759f75aa8a860e010684ec
Trusty <123142834+IvanViegas@users.noreply.github.com>
2024-11-30 18:47:22 -0300

    leer desde excel y subir cambios del map (en una nueva sheet)

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -3,6 +3,7 @@ const { mapPlayers } = require('../resources/playerRepository/players');
 const file = reader.readFile('./resources/playerRepository/ExcelRepository/players.xlsx')
 const sheets = file.SheetNames
 
+//Mueve todos los players de la lista de excel al map
 pushToMap()
 function pushToMap(){
    for(let i = 0; i < sheets.length; i++)
@@ -13,6 +14,27 @@ function pushToMap(){
              mapPlayers.set(res.Player, res.smashID)
           })
        }
+}
+
+//Tiene que mejorarse, el algoritmo crea nuevas hojas en vez de añadir los que no están.
+function mapToExcel(){
+   function playerInMap(player, smashID){
+      this.player = player;
+      this.smashID = smashID;
+  }
+  
+  const data = []
+  mapPlayers.forEach((value, key) => {
+      // Crear una nueva instancia de PlayerInMap y agregarla al array
+      data.push(new playerInMap(key, value));
+  });
+  
+  const ws = reader.utils.json_to_sheet(data)
+  
+  reader.utils.book_append_sheet(file,ws)
+  
+  // Writing to our file
+  reader.writeFile(file,'./resources/playerRepository/ExcelRepository/players.xlsx')
 }
 
 
