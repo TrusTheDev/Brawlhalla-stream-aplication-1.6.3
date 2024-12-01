3df7aece06419d651ec62de5343c6b2dda99f652
Trusty <123142834+IvanViegas@users.noreply.github.com>
2024-12-01 16:33:07 -0300

    refactor
    map obtiene información de la tabla de excel

--- resources/playerRepository/players.js
+++ resources/playerRepository/players.js
@@ -1,24 +1,5 @@
-const mapPlayers = new Map([
-    ["Power", 327992], //1
-    ["Neuz", 322393],//2
-    ["Nitram", 1569801],//3
-    ["Agmed", 1999846],//4
-    ["Logic", 1025433],//5
-    ["Lauta", 2184467],//6
-    ["Azasuh", 1611895],//7
-    ["Odnu", 2220340],///8
-    ["4nto", 379003],///9
-    ["Rec", 1092744],///10
-    ["Cyclonix", 2000389],///10
-    ["Joako", 892260],///10
-    ["Lux", 530089],///10
-    ["Fiend", 312642]///10
-
-
-    
-
-    
-])
+//gets values from players.xlsx
+const mapPlayers = new Map([])
 
 module.exports = {
     mapPlayers
