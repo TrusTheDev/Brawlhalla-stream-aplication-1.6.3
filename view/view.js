68a5a9f5307c15bff85ac19727a027b3c53758b4
Trusty <123142834+IvanViegas@users.noreply.github.com>
2024-12-01 16:35:20 -0300

    Add files via upload

--- view/view.js
+++ view/view.js
@@ -1,11 +1,13 @@
 const { question, salirDelPrograma, initSingleModeView, displayLogo, putPlayer, editPlayer, deletePlayer, about, getAllplayersOption } = require('../controllers/viewController')
+const { pushToMap } = require('../controllers/mapController')
 async function main() {
+    
     console.log("Iniciando el programa...");
-    //Arreglar el logo
     displayLogo()
     console.log("Bienvenido a ballstats!, porfavor elija una opción \n 0:salir del programa \n 1:Iniciar programa \n 2: listar jugadores \n 3: agregar jugador \n 4: editar agregar \n 5: eliminar usuario \n 6: about \n")
     let option = ""
     while(option != '0'){
+        pushToMap()
         option = await question("");
     switch (option){
         
@@ -31,16 +33,19 @@ async function main() {
             main()
             break;
         case '3': 
+            //deprecated
             console.log("Agregar jugador " + option);
             await putPlayer();
             main()
             break;
         case '4':
+            //deprecated
             console.log("Editar jugador " + option); 
             await editPlayer();
             main()
             break;
         case '5': 
+            //deprecated
             console.log("Eliminar usuario " + option);
             await deletePlayer();
             main()
@@ -52,6 +57,7 @@ async function main() {
             break;  
       
         default:    
+            //deprecated
             console.log("opción inválida");
             process.exit(0);
         }
